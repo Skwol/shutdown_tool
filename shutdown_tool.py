88269ce1daaf73e929ee33563cb99bedc7d1d2e6
ivan.rozhkov <rozhkov92@gmail.com>
2017-09-09 23:34:53 +0300

    Tool for windows PC shutdown timer

---
+++ shutdown_tool.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import Tkinter
+import tkMessageBox
+import subprocess
+
+
+def shutdown():
+    try:
+        minutes_to_shut = int(minutes.get())
+        subprocess.call(u'shutdown -s -t {}'.format(minutes_to_shut))
+    except ValueError:
+        error_message = u'Вы ввели неверное колличество минут. Введите целое число.'
+        tkMessageBox.showwarning(title=u'Ошибка', message=error_message)
+
+
+def abort():
+    subprocess.call('shutdown -a')
+
+root = Tkinter.Tk()
+
+minutes = Tkinter.Entry(root)
+minutes.grid(row=1, column=1, columnspan=2)
+Tkinter.Button(root, text=u'Выключить компютер', command=shutdown).grid(row=2, column=1, columnspan=2)
+Tkinter.Button(root, text=u'Отменить выключение', command=abort).grid(row=3, column=1, columnspan=2)
+
+if __name__ == '__main__':
+    root.mainloop()
